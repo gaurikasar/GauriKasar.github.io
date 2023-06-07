f4ce0f42dd88f5edbeadc76070b655c4decab697
gauri kasar <gaurikasar333@gmail.com>
2023-06-07 11:23:50 -0700

    added contact form and education details to index.html

--- Javascript/project.js
+++ Javascript/project.js
@@ -1,34 +1,5 @@
 /** @format */
 
-var project_description1 = document.querySelector("#project-id1");
-var displayDesc1 = function (e) {
-  let desc_display = project_description1.style.display;
-  if (desc_display == "block") {
-    project_description1.style.display = "none";
-  } else {
-    project_description1.style.display = "block";
-  }
-};
-
-var project_description2 = document.querySelector("#project-id2");
-var displayDesc2 = function (e) {
-  let desc_display = project_description2.style.display;
-  if (desc_display == "block") {
-    project_description2.style.display = "none";
-  } else {
-    project_description2.style.display = "block";
-  }
-};
-
-var project_description3 = document.querySelector("#project-id3");
-var displayDesc3 = function (e) {
-  let desc_display = project_description3.style.display;
-  if (desc_display == "block") {
-    project_description3.style.display = "none";
-  } else {
-    project_description3.style.display = "block";
-  }
-};
 const toggleDescription = (card) => {
   card.classList.toggle("active");
 
